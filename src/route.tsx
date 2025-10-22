8b605f1a3b3cd22832d9c509de44bb87dfb42fb2
rasran <rasyazahran46@gmail.com>
2025-10-22 13:17:01 +0700

    feat: implement login alert and separate dashboards for each role

--- src/route.tsx
+++ src/route.tsx
@@ -5,6 +5,10 @@ import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import SigninUserPage from './pages/signin/signinUser'
 import SigninAdminPage from './pages/signin/signinAdmin'
+import AdminDashboard from './pages/dasboard/admin/dasboard'
+import PmDashboard from './pages/dasboard/pm/dasboard'
+import DevDashboard from './pages/dasboard/dev/dasboard'
+import { ProtectedRoute } from "./components/ProtectedRoute"
 
 const rootElement = document.getElementById('root') as HTMLElement;
 
@@ -14,6 +18,31 @@ createRoot(rootElement).render(
         <Routes>
           <Route path="/" element={<SigninUserPage />} />
           <Route path='/admin/signin' element={<SigninAdminPage />}></Route>
+           {/* === DASHBOARD ROUTES === */}
+        <Route
+          path="/admin/dashboard"
+          element={
+            <ProtectedRoute allowedRoles={["admin"]}>
+              <AdminDashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/project-manager/dashboard"
+          element={
+            <ProtectedRoute allowedRoles={["project_manager"]}>
+              <PmDashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/developer/dashboard"
+          element={
+            <ProtectedRoute allowedRoles={["developer"]}>
+              <DevDashboard />
+            </ProtectedRoute>
+          }
+        />
         </Routes>
     </BrowserRouter>
   </StrictMode>,
